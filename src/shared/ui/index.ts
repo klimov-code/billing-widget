012b48b058d88bdfe024b85b15b1f6820dbaecaa
Ilia Klimov <therapyspb@yandex.ru>
2022-05-05 16:26:27 +0400

    removed: ui components

--- src/shared/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button } from './button';
-export { Card } from './card';
-export { Checkbox } from './checkbox';
-export { Divider } from './divider';
-export { Skeleton } from './skeleton';

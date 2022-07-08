e6bc28989624799a5ea50d44de7ff05708b4e8ba
Ilia Klimov <therapyspb@yandex.ru>
2022-07-08 20:57:42 +0400

    changed: cost string to variable

--- src/entities/cost/model.ts
+++ src/entities/cost/model.ts
@@ -5,7 +5,7 @@ import { Code } from '@app/shared/types/common';
 export type Entity = Record<Code, number>;
 
 export const $cost = createStore<Entity>({
-  'product.domain': 2.3,
-  'product.server': 1.1,
-  'product.forwarder': 1.6,
+  [Code.domain]: 2.3,
+  [Code.server]: 1.1,
+  [Code.forwarder]: 1.6,
 });

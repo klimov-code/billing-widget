3057d119fc8b468a3d4de522ba38b2f3c6c5d1ca
Ilia Klimov <therapyspb@yandex.ru>
2022-08-18 13:55:10 +0400

    changed: rounding

--- src/shared/lib/convertToString.ts
+++ src/shared/lib/convertToString.ts
@@ -1,1 +1,1 @@
-export const convertToString = (number: number) => ((number + Number.EPSILON) / 100).toFixed(2);
+export const convertToString = (number: number) => ((number + Number.EPSILON) / 100).toFixed(3);

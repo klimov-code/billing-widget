3dcb2646ba725f9eb5a981543c05263229a2f893
Ilia Klimov <klimov@ttfb.dev>
2023-05-19 19:26:05 +0400

    updated: test cases about billing & cost

--- src/features/billing-list/model.test.ts
+++ src/features/billing-list/model.test.ts
@@ -4,31 +4,30 @@ import { describe, it } from 'vitest';
 import { timeFactor } from '@app/shared/lib/timeFactor';
 import { Code } from '#types/common';
 
-import { $billingCount, $billingList, BillingInfo, billingListUpdated, Entity } from './model';
+import { $billingCount, $billingList, billingListUpdated, Entity } from './model';
 
 describe('billing-list model', () => {
   it('updates billing list', async () => {
-    const info: BillingInfo = [
-      {
-        'product.domain': 1,
-        'product.server': 2,
-        'product.forwarder': 3,
-      },
-      {
-        'product.domain': 1,
-        'product.server': 2,
-        'product.forwarder': 3,
-      },
-      'month',
-    ];
+    const resource = {
+      [Code.domain]: 1,
+      [Code.server]: 2,
+      [Code.forwarder]: 3,
+    };
+    const cost = {
+      [Code.domain]: 1,
+      [Code.server]: 2,
+      [Code.forwarder]: 3,
+    };
+    const period = 'month';
+    const products = Object.entries({
+      [Code.domain]: 'Domains',
+      [Code.server]: 'Servers',
+      [Code.forwarder]: 'Forwarders',
+    }) as [Code, string][];
 
-    const resource = info[0];
-    const cost = info[1];
-    const period = info[2];
-    const products = ['product.domain', 'product.server', 'product.forwarder'] as Code[];
-    const result = Array.from<Code, Entity>(products, (code) => ({
+    const result = Array.from<[Code, string], Entity>(products, ([code, name]) => ({
       code,
-      name: 'Domains',
+      name,
       quantity: resource[code],
       price: cost[code] * timeFactor[period],
       price_unit: 'pcs',
@@ -38,7 +37,7 @@ describe('billing-list model', () => {
 
     const scope = fork();
 
-    await allSettled(billingListUpdated, { scope, params: info });
+    await allSettled(billingListUpdated, { scope, params: [resource, cost, period] });
     expect(scope.getState($billingCount)).toBe(3);
     expect(scope.getState($billingList)).toStrictEqual(result);
   });
--- src/features/total-cost/model.test.ts
+++ src/features/total-cost/model.test.ts
@@ -1,27 +1,33 @@
 import { allSettled, fork } from 'effector';
 import { describe, it } from 'vitest';
 
-import { $totalCost, TotalCostInfo, totalCostUpdated } from './model';
+import { timeFactor } from '@app/shared/lib/timeFactor';
+import { Code } from '#types/common';
 
-describe('billing-list model', () => {
-  it('updates billing list', async () => {
-    const info: TotalCostInfo = [
-      {
-        'product.domain': 1,
-        'product.server': 2,
-        'product.forwarder': 3,
-      },
-      {
-        'product.domain': 2.3,
-        'product.server': 1.1,
-        'product.forwarder': 1.6,
-      },
-      'month',
-    ];
+import { $totalCost, totalCostUpdated } from './model';
+
+describe('total-cost model', () => {
+  it('updates total cost', async () => {
+    const resource = {
+      [Code.domain]: 1,
+      [Code.server]: 2,
+      [Code.forwarder]: 3,
+    };
+    const cost = {
+      [Code.domain]: 2,
+      [Code.server]: 1,
+      [Code.forwarder]: 3,
+    };
+    const period = 'month';
+
+    const total =
+      resource[Code.domain] * cost[Code.domain] * timeFactor[period] +
+      resource[Code.server] * cost[Code.server] * timeFactor[period] +
+      resource[Code.forwarder] * cost[Code.forwarder] * timeFactor[period];
 
     const scope = fork();
 
-    await allSettled(totalCostUpdated, { scope, params: info });
-    expect(scope.getState($totalCost)).toBe(3);
+    await allSettled(totalCostUpdated, { scope, params: [resource, cost, period] });
+    expect(scope.getState($totalCost)).toBe(total);
   });
 });
